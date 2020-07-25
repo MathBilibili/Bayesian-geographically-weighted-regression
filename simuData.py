dc09825c594e90eb4d1c6a97d947bbcd35d444f1
Yang LIU <nkstat@outlook.com>
2020-07-25 11:56:15 +0100

    Update simuData.py

--- simuData.py
+++ simuData.py
@@ -27,10 +27,10 @@ for i in range(5):
     for j in range(5):
         x=i+1
         y=j+1
-        phi=[1,0.5*np.sqrt((x-5.5)**2+(y-5.5)**2),0.1*((x-5.5)+(y-5.5))]
-        theta = float(np.random.uniform(0,0.5,1))
-        for n in range(20):
-            covariate=[1,float(np.random.uniform(0,1,1)),float(np.random.uniform(0,2,1))]
+        phi=[1,0.5*np.sqrt((x-5.5)**2+(y-5.5)**2),0.1*(np.sin(x)+np.cos(y)+4)]
+        theta = float(np.random.uniform(0,0.1,1))
+        for n in range(100):
+            covariate=[1,float(np.random.uniform(0,1,1)),float(np.random.uniform(2,7,1))]
             outcome = int(rv_binom(offset,covariate,phi,theta))
             re = re.append({'x':x,'y':y,'outcome':outcome,'offset':offset,'x1':covariate[0],'x2':covariate[1],'x3':covariate[2]},ignore_index=True)
             true_param = true_param.append({'x':x,'y':y,'phi1':phi[1],'phi2':phi[2],'theta':theta},ignore_index=True)
