ce8413657065f6e9f6f3559671ccc45301fb5797
Yang LIU <nkstat@outlook.com>
2020-08-02 11:28:15 +0100

    Update simuData.py

--- simuData.py
+++ simuData.py
@@ -23,14 +23,14 @@ re = pd.DataFrame({'x':[],'y':[],'outcome':[],'offset':[],'x1':[],'x2':[],'x3':[
 true_param = pd.DataFrame({'x':[],'y':[],'phi1':[],'phi2':[],'theta':[]})
 offset=1
 
-for i in range(5):
-    for j in range(5):
+for i in range(10):
+    for j in range(10):
         x=i+1
         y=j+1
-        phi=[1,0.5*np.sqrt((x-5.5)**2+(y-5.5)**2),0.1*(np.sin(x)+np.cos(y)+4)]
-        theta = float(np.random.uniform(0,0.1,1))
-        for n in range(100):
-            covariate=[1,float(np.random.uniform(0,1,1)),float(np.random.uniform(2,7,1))]
+        phi=[3,0.02*np.sqrt((x)**2+(y)**2),0.05*(np.sin(x/2)+np.cos(y/2)+4)]
+        theta = float(np.random.uniform(0.5,0.6,1))
+        for n in range(20):
+            covariate=[1,float(np.random.uniform(0,2,1)),float(np.random.uniform(2,7,1))]
             outcome = int(rv_binom(offset,covariate,phi,theta))
             re = re.append({'x':x,'y':y,'outcome':outcome,'offset':offset,'x1':covariate[0],'x2':covariate[1],'x3':covariate[2]},ignore_index=True)
             true_param = true_param.append({'x':x,'y':y,'phi1':phi[1],'phi2':phi[2],'theta':theta},ignore_index=True)
